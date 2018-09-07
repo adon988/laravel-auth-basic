22681b7aa28375d591e6c7e6204af4a9db4662ea
adam <adon988@gmail.com>
2018-09-07 21:37:32 +0800

    This is command2

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,10 +1,15 @@
 @servers(['web' => 'localhost'])
- 
+
+@setup
+$now = new DateTime();
+
+$commit = isset($myparam) ? $myparam : "commit at time".$now;
+@endsetup
+
 @task('deploy')
     git add -A
     git commit -m"{{$commit}}"
     git push origin master
-    
 @endtask
 
 @task('pull')
