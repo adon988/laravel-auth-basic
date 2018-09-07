cd83f462dd554e7efd51e2ff42bca4da6aec732f
adam <adon988@gmail.com>
2018-09-07 22:35:32 +0800

    測試story 2018-09-07 14:35:22

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,21 +1,22 @@
 
 @servers(['web' => 'localhost'])
+
 @setup
     $now = date("Y-m-d H:i:s");
 @endsetup
-@story('deploy')
+
+@story('deploy', ['on' => 'web', 'confirm' => true])
     add
     commit
     push
-    echo '完成deploy';
 @endstory
 
-@task('add')
+@task('add', ['on' => 'web', 'confirm' => true])
     git add -A
     echo '加入變更檔案';
 @endtask
 
-@task('commit')
+@task('commit', ['on' => 'web', 'confirm' => true])
     @if($commit)
         git commit -m"{{$commit}} {{$now}}" 
         echo '傳入參數至';
@@ -27,7 +28,7 @@
     echo '執行commit';
 @endtask
 
-@task('push')
+@task('push', ['on' => 'web', 'confirm' => true])
     git push origin master
     echo '推送執行完畢';
 @endtask
